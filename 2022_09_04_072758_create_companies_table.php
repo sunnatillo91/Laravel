3fe03374a029b15611c82f0a47eebac71cf6a28b
Sunnatillo <97885433+sunnatillo91@users.noreply.github.com>
2022-09-04 12:51:14 +0500

    New lesson added

---
+++ 2022_09_04_072758_create_companies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',255);
+            $table->string('address')->nullable();  //nullable() yoziladigan bo'lsa o'sha ustunni to'ldirish ixtiyoriy bo'ladi
+            $table->string('phone',20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
